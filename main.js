ecfe2da3ffda0fc6aeb695ea38b8e8e1d13bc39b
Mauri Rodríguez <maurinightmare27@gmail.com>
2022-07-26 18:19:22 -0500

    Car Shop added

--- main.js
+++ main.js
@@ -2,17 +2,47 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu  = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
-const mobileMenu  = document.querySelector('.mobile-menu');
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
 
+const mobileMenu  = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarIcon.addEventListener('click', toggleCarAside);
+
 
 function toggleDesktopMenu(){
+    const isAsideOpen = !aside.classList.contains('inactive');
+    
+    if(isAsideOpen){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
+
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+    const isAsideOpen = !aside.classList.contains('inactive');
+    
+    if(isAsideOpen){
+        aside.classList.add('inactive');
+    }
 
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarAside(){
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    if(isDesktopMenuOpen){
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive')
 }
